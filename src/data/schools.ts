e81814e689ede883ec4cabba6cbd750643f80197
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-23 19:38:08 +0700

    refactor: change variable to camelCase

--- src/data/schools.ts
+++ src/data/schools.ts
@@ -1,14 +1,14 @@
 type Schools = {
   id: number;
   name: string;
-  address: string;
-  province_id: string;
-  province: string;
-  city_id: string;
-  city: string;
-  subdistrict_id: string;
-  subdistrict: string;
   npsn: string;
+  address: string;
+  provinceId: string;
+  province: string;
+  cityId: string;
+  city: string;
+  subdistrictId: string;
+  subdistrict: string;
 };
 
 export const schools: Schools[] = [
@@ -17,11 +17,11 @@ export const schools: Schools[] = [
     name: "SMAN 51 JAKARTA",
     npsn: "20103312",
     address: "JL. BATU AMPAR 3 CONDET",
-    province_id: "010000  ",
+    provinceId: "010000  ",
     province: "Prov. D.K.I. Jakarta",
-    city_id: "016400  ",
+    cityId: "016400  ",
     city: "Kota Jakarta Timur",
-    subdistrict_id: "016405  ",
+    subdistrictId: "016405  ",
     subdistrict: "Kec. Kramat Jati",
   },
   {
@@ -29,11 +29,11 @@ export const schools: Schools[] = [
     name: "SMAN 62 JAKARTA",
     npsn: "20103306",
     address: "JL. RAYA BOGOR KM.20",
-    province_id: "010000  ",
+    provinceId: "010000  ",
     province: "Prov. D.K.I. Jakarta",
-    city_id: "016400  ",
+    cityId: "016400  ",
     city: "Kota Jakarta Timur",
-    subdistrict_id: "016405  ",
+    subdistrictId: "016405  ",
     subdistrict: "Kec. Kramat Jati",
   },
   {
@@ -41,11 +41,11 @@ export const schools: Schools[] = [
     name: "SMAN 104 JAKARTA",
     npsn: "20103306",
     address: "JL. H. TAIMAN BARAT KEL.GEDONG,JAKARTA TIMUR",
-    province_id: "010000  ",
+    provinceId: "010000  ",
     province: "Prov. D.K.I. Jakarta",
-    city_id: "016400  ",
+    cityId: "016400  ",
     city: "Kota Jakarta Timur",
-    subdistrict_id: "016401  ",
+    subdistrictId: "016401  ",
     subdistrict: "Kec. Pasar Rebo",
   },
 ];
