e05af54c2224a938532365dc1581c3e20b0c0df4
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-20 12:27:59 +0700

    refactor: text home to school api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("School API");
+});
 
-export default app
+export default app;
