3c5cfc098bd200bc5fd99483758a950078be3270
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-21 06:51:06 +0700

    feat: get all provinces

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
+import { prisma } from "./utils/db";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("School API");
+  return c.json({
+    message: "School API",
+    schools: "/schools",
+  });
+});
+
+app.get("/provinces", async (c) => {
+  const provinces = await prisma.province.findMany();
+  console.log(provinces);
+  return c.json(provinces);
 });
 
 export default app;
