91cde4ee573c90d8e96599cea6c2dd66c177f81e
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-21 07:30:32 +0700

    feat: implement post province

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Hono } from "hono";
-import { prisma } from "./utils/db";
-
 import { cors } from "hono/cors";
+
+import { zValidator } from "@hono/zod-validator";
+import z from "zod";
+
+import { prisma } from "./utils/db";
+import createSlug from "./utils/slug";
+import { PrismaClientKnownRequestError } from "./generated/prisma/runtime/library";
 
 const app = new Hono();
 
@@ -39,5 +44,46 @@ app.get("/provinces/:slug", async (c) => {
 
   return c.json(province);
 });
+
+app.post(
+  "/provinces",
+  zValidator(
+    "json",
+    z.object({
+      name: z.string(),
+    })
+  ),
+
+  async (c) => {
+    const body = c.req.valid("json");
+
+    try {
+      const newProvince = await prisma.province.create({
+        data: {
+          name: body.name,
+          slug: createSlug(body.name),
+        },
+      });
+
+      return c.json({
+        message: "Successfully created province!",
+        newProvice: newProvince,
+      });
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        c.status(409);
+        return c.json({
+          message: `Provinsi dengan nama '${body.name}' sudah ada.`,
+        });
+      }
+
+      c.status(500);
+      return c.json({ message: "Terjadi kesalahan pada server." });
+    }
+  }
+);
 
 export default app;
