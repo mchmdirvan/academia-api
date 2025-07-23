13ffbc112b90d35120fb9880c1637df102278dfa
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-23 09:45:45 +0700

    feat: add more path on index

--- src/routes/roots.ts
+++ src/routes/roots.ts
@@ -3,6 +3,11 @@ import { Hono } from "hono";
 export const rootRoute = new Hono().get("/", (c) => {
   return c.json({
     message: "School API",
-    schools: "/schools",
+    path: {
+      schools: "/schools",
+      provinces: "/provinces",
+      cities: "/cities",
+      distirct: "/districts",
+    },
   });
 });
